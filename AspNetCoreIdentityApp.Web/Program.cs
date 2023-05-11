2229cff38187c1b343dded82975b8f0d88e75dfa
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-05-11 03:21:23 -0700

    Add project files.

---
+++ AspNetCoreIdentityApp.Web/Program.cs
@@ -0,0 +1,159 @@
+using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreIdentityApp.Web.Extenisons;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using AspNetCoreIdentityApp.Web.OptionsModels;
+using Microsoft.Extensions.DependencyInjection;
+using AspNetCoreIdentityApp.Web.Services;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.Authentication;
+using AspNetCoreIdentityApp.Web.ClaimProviders;
+using AspNetCoreIdentityApp.Web.Requirements;
+using Microsoft.AspNetCore.Authorization;
+using AspNetCoreIdentityApp.Web.Seeds;
+using AspNetCoreIdentityApp.Web.PermissionsRoot;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConn"));
+});
+
+//SecurityStamp Configuration
+builder.Services.Configure<SecurityStampValidatorOptions>(options =>
+{
+    options.ValidationInterval = TimeSpan.FromMinutes(30);
+});
+
+builder.Services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Directory.GetCurrentDirectory()));
+
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+
+//Extention method 
+builder.Services.AddIdentityWithExt();
+
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IClaimsTransformation, UserClaimProvider>();
+builder.Services.AddScoped<IAuthorizationHandler, ExchangeExpireRequirementHandler>();
+builder.Services.AddScoped<IAuthorizationHandler, ViolenceRequirementHandler>();
+
+builder.Services.AddAuthorization(options =>
+{
+
+    options.AddPolicy("AnkaraPolicy", policy =>
+    {
+        policy.RequireClaim("city", "ankara");
+
+
+    });
+
+    options.AddPolicy("ExchangePolicy", policy =>
+    {
+        policy.AddRequirements(new ExchangeExpireRequirement());
+
+
+    });
+
+    options.AddPolicy("ViolencePolicy", policy =>
+    {
+        policy.AddRequirements(new ViolenceRequirement() {  ThresholdAge=18});
+
+
+    });
+
+    options.AddPolicy("OrderPermissionReadAndDelete", policy =>
+    {
+       
+        policy.RequireClaim("permission", Permissions.Order.Read);
+        policy.RequireClaim("permission", Permissions.Order.Delete);
+        policy.RequireClaim("permission", Permissions.Stock.Delete);
+
+    });
+
+
+
+    options.AddPolicy("Permissions.Order.Read", policy =>
+    {
+
+        policy.RequireClaim("permission", Permissions.Order.Read);
+     
+
+    });
+
+    options.AddPolicy("Permissions.Order.Delete", policy =>
+    {
+
+        policy.RequireClaim("permission", Permissions.Order.Delete);
+
+
+    });
+
+
+    options.AddPolicy("Permissions.Stock.Delete", policy =>
+    {
+
+        policy.RequireClaim("permission", Permissions.Stock.Delete);
+
+
+    });
+
+
+
+
+});
+
+//Cookie optimization
+builder.Services.ConfigureApplicationCookie(opt =>
+{
+    var cookieBuilder = new CookieBuilder();
+
+    cookieBuilder.Name = "UdemyAppCookie";
+    opt.LoginPath = new PathString("/Home/Signin");
+    opt.LogoutPath = new PathString("/Member/logout");
+    opt.AccessDeniedPath = new PathString("/Member/AccessDenied");
+    opt.Cookie = cookieBuilder;
+    opt.ExpireTimeSpan = TimeSpan.FromDays(60);
+
+    //istifadeci 60 gun erzinde giris elese cookie-nin omru 60 gun uzancax "opt.SlidingExpiration" ture elediyimiz zaman.
+    opt.SlidingExpiration = true;
+});
+
+var app = builder.Build();
+
+using (var scope= app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<AppRole>>();
+
+   //await  PermissionSeed.Seed(roleManager);
+}
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ AspNetCoreIdentityApp.Web/Services/EmailService.cs
@@ -0,0 +1,51 @@
+﻿using AspNetCoreIdentityApp.Web.OptionsModels;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+
+namespace AspNetCoreIdentityApp.Web.Services
+{
+    public class EmailService : IEmailService
+    {
+
+        private readonly EmailSettings _emailSettings;
+
+        public EmailService(IOptions<EmailSettings> options)
+        {
+            _emailSettings = options.Value;
+        }
+
+        public async Task SendResetPasswordEmail(string resetPasswordEmailLink, string ToEmail)
+        {
+            //Smtp Configuration
+            var smtpClient = new SmtpClient();
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Host = _emailSettings.Host;
+            smtpClient.Port = 587;
+            smtpClient.Credentials = new NetworkCredential(_emailSettings.Email, _emailSettings.Password);
+            smtpClient.EnableSsl = true;
+
+            //Message side
+            var mailMessage = new MailMessage();
+
+            //hansi email adresinnen mesaj gedecey onu bildirir;
+            mailMessage.From = new MailAddress(_emailSettings.Email);
+
+            //Gonderilecey message
+            mailMessage.To.Add(ToEmail);
+
+            //Message subject configuration
+            mailMessage.Subject = "LocalHost | Sifre sifirlama linki";
+            mailMessage.Body = @$"<h4>Sifrenizi yenilem ucun asagidaki linke tiklayiniz
+                                    <p>
+                                       <a href='{resetPasswordEmailLink}'>Sifre yenileme link</a>
+                                    </p>
+                                </h4>";
+            mailMessage.IsBodyHtml = true;
+
+            //send
+            await smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
